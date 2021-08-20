31bde17612568428f65e94c9a10a3acebb899d11
Anton Petrov <anton.a.petrov@gmail.com>
2021-08-20 10:05:13 +0300

    Added multipltaform push to docker hub

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -5,11 +5,15 @@ set -e
 use_tag="antonapetrov/uvicorn-gunicorn-fastapi:$NAME"
 use_dated_tag="${use_tag}-$(date +%F)"
 
-bash scripts/build.sh
+DOCKERFILE="$NAME"
 
-docker tag "$use_tag" "$use_dated_tag"
+if [ "$NAME" == "latest" ] ; then
+    DOCKERFILE="python3.9"
+fi
 
 bash scripts/docker-login.sh
 
-docker push "$use_tag"
-docker push "$use_dated_tag"
+# docker buildx create --use --platform linux/arm64/v8,linux/amd64,linux/ppc64le
+docker buildx build --platform linux/amd64,linux/arm64 -t "$use_tag" --file "./docker-images/${DOCKERFILE}.dockerfile" --push "./docker-images/"
+
+# docker tag "$use_tag"
